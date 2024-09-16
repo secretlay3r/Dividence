f077d8293f9af1db9c098fdcca3b740b6acee886
secretlay3r <80585554+secretlay3r@users.noreply.github.com>
2024-09-16 17:55:11 +0300

    Update norecoil.py

--- norecoil.py
+++ norecoil.py
@@ -7,10 +7,7 @@ import threading
 horizontal_range = 2
 vertical_range = 2
 downward_range = 4
-min_firerate = 0.04
-max_firerate = 0.05
 compensation_interval = 0.03
-
 enabled = False
 
 def is_mouse_down():
@@ -21,28 +18,20 @@ def toggle_norecoil():
     global enabled
     enabled = not enabled
     if enabled:
-        start_norecoil_loop()
+        threading.Thread(target=start_norecoil_loop, daemon=True).start()
 
 def start_norecoil_loop():
-    def loop():
-        global enabled
-        while True:
-            if is_mouse_down() and enabled:
-                while is_mouse_down():
-                    offset_const = 1000
-                    
-                    horizontal_offset = random.uniform(-horizontal_range * offset_const, horizontal_range * offset_const) / offset_const
-                    vertical_offset = random.uniform(-vertical_range * offset_const, vertical_range * offset_const) / offset_const
-                    downward_offset = random.uniform(3, downward_range * offset_const) / offset_const
+    while enabled:
+        if is_mouse_down():
+            while is_mouse_down():
+                apply_recoil_compensation()
+                time.sleep(compensation_interval)
+            time.sleep(0.001)
+        else:
+            time.sleep(0.01)
 
-                    ctypes.windll.user32.mouse_event(0x1, int(horizontal_offset), int(vertical_offset + downward_offset), 0, 0)
-                    time.sleep(compensation_interval)
-                
-                time.sleep(0.001)
-            else:
-                time.sleep(0.01)
+def apply_recoil_compensation():
+    horizontal_offset = random.uniform(-horizontal_range, horizontal_range)
+    vertical_offset = random.uniform(-vertical_range, vertical_range) + downward_range
 
-    threading.Thread(target=loop, daemon=True).start()
-
-if __name__ == "__main__":
-    start_norecoil_loop()
+    ctypes.windll.user32.mouse_event(0x0001, int(horizontal_offset), int(vertical_offset), 0, 0)
