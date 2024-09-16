91b34611ab27337d1a8379257271b26322892ef7
secretlay3r <80585554+secretlay3r@users.noreply.github.com>
2024-09-16 17:55:23 +0300

    Update triggerbot.py

--- triggerbot.py
+++ triggerbot.py
@@ -7,21 +7,18 @@ import threading
 import random
 
 pyautogui.FAILSAFE = False
-
-VK_XBUTTON4 = 0x05
-
 enabled = False
 target_colors = []
 scan_area_size = 20
-monitor_resolution = (1920, 1080)  # Default res
+monitor_resolution = (1920, 1080)  # Default resolution
 
 color_ranges = {
-    "ORANGE": {'r': (75, 108), 'g': (140, 244), 'b': (244, 255)},  # #C57824
-    "YELLOW": {'r': (78, 133), 'g': (237,255), 'b': (237,255)},  # #FFFF3D
-    "PURPLE": {'r': (144, 255), 'g': (60, 99), 'b': (194, 255)},  # #FF33FF
-    "RED": {'r': (80, 135), 'g': (100, 130), 'b': (247, 255)},  # #FF3132
-    "GREEN": {'r': (30, 97), 'g': (240, 255), 'b': (30, 110)},  # #2EFB00
-    "CYAN": {'r': (246, 255), 'g': (246, 255), 'b': (66, 100)},  # #00FFFF
+    "ORANGE": {'r': (75, 108), 'g': (140, 244), 'b': (244, 255)},
+    "YELLOW": {'r': (78, 133), 'g': (237, 255), 'b': (237, 255)},
+    "PURPLE": {'r': (144, 255), 'g': (60, 99), 'b': (194, 255)},
+    "RED": {'r': (80, 135), 'g': (100, 130), 'b': (247, 255)},
+    "GREEN": {'r': (30, 97), 'g': (240, 255), 'b': (30, 110)},
+    "CYAN": {'r': (246, 255), 'g': (246, 255), 'b': (66, 100)},
 }
 
 def set_monitor_resolution(width, height):
@@ -44,7 +41,7 @@ def set_scan_area_size(new_size):
 
 def run_triggerbot():
     sct = mss.mss()
-    lower_delay, upper_delay = 0.005, 0.02  # Randomized delay range between shots
+    lower_delay, upper_delay = 0.005, 0.02
     monitor = get_monitor_region()
 
     while enabled:
@@ -52,27 +49,16 @@ def run_triggerbot():
         for color_range in target_colors:
             if detect_color_in_range(screenshot, color_range):
                 shoot()
-                time.sleep(random.uniform(lower_delay, upper_delay))  # Random delay between clicks
+                time.sleep(random.uniform(lower_delay, upper_delay))
         time.sleep(0.001)
 
 def get_monitor_region():
-    if monitor_resolution == (1920, 1080):
-        monitor = {
-            "top": 540 - scan_area_size // 2,
-            "left": 960 - scan_area_size // 2,
-            "width": scan_area_size,
-            "height": scan_area_size
-        }
-    elif monitor_resolution == (1280, 720):
-        monitor = {
-            "top": int(540 * 720 / 1080) - scan_area_size // 2,
-            "left": int(960 * 1280 / 1920) - scan_area_size // 2,
-            "width": int(scan_area_size * 1280 / 1920),
-            "height": int(scan_area_size * 720 / 1080)
-        }
-    else:
-        raise ValueError("Unsupported resolution")
-    return monitor
+    return {
+        "top": monitor_resolution[1] // 2 - scan_area_size // 2,
+        "left": monitor_resolution[0] // 2 - scan_area_size // 2,
+        "width": scan_area_size,
+        "height": scan_area_size
+    }
 
 def detect_color_in_range(screenshot, color_range):
     r_min, r_max = color_range['r']
@@ -90,23 +76,3 @@ def detect_color_in_range(screenshot, color_range):
 def shoot():
     ctypes.windll.user32.mouse_event(0x0002, 0, 0, 0, 0)  # Left button down
     ctypes.windll.user32.mouse_event(0x0004, 0, 0, 0, 0)  # Left button up
-
-def get_key_state(key_code):
-    return ctypes.windll.user32.GetAsyncKeyState(key_code)
-
-def main():
-    global enabled
-    frame_rate = 900
-    interval = 1.0 / frame_rate
-
-    while True:
-        if get_key_state(VK_XBUTTON4) & 0x8000:
-            if not enabled:
-                toggle_triggerbot(True)
-            time.sleep(0.1)  # Prevent rapid toggling
-        elif not (get_key_state(VK_XBUTTON4) & 0x8000) and enabled:
-            toggle_triggerbot(False)
-        time.sleep(interval)
-
-if __name__ == "__main__":
-    main()
