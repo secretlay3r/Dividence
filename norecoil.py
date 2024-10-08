9f731950aa49e02b76abac7b96dc5feca8654df8
secretlay3r <80585554+secretlay3r@users.noreply.github.com>
2024-10-08 18:38:10 +0300

    Update norecoil.py

--- norecoil.py
+++ norecoil.py
@@ -10,6 +10,9 @@ downward_range = 4.0
 compensation_interval = 0.03
 enabled = False
 
+chance = 0.1
+x_rand_move = 3.2
+
 def is_mouse_down():
     lmb_state = win32api.GetKeyState(0x01)
     return lmb_state < 0
@@ -31,9 +34,12 @@ def start_norecoil_loop():
             time.sleep(0.01)
 
 def apply_recoil_compensation():
-    horizontal_offset = random.uniform(-horizontal_range, horizontal_range)
-    vertical_offset = random.uniform(-vertical_range, vertical_range) + downward_range
+    if random.random() < chance:
+        horizontal_offset = random.uniform(-x_rand_move, x_rand_move)
+    else:
+        horizontal_offset = random.uniform(-horizontal_range, horizontal_range)
 
+    vertical_offset = random.uniform(-vertical_range, vertical_range) + downward_range
     ctypes.windll.user32.mouse_event(0x0001, int(horizontal_offset), int(vertical_offset), 0, 0)
 
 def set_compensation_speed(new_interval):
@@ -47,3 +53,11 @@ def set_x_recoil(new_horizontal_range):
 def set_y_recoil(new_vertical_range):
     global vertical_range
     vertical_range = new_vertical_range
+
+def set_chance(upd_chance):
+    global chance
+    chance = upd_chance
+
+def set_x_rand_move(new_x_rand_move):
+    global x_rand_move
+    x_rand_move = new_x_rand_move
